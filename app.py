68beeec9e1f65c6b24d2c83a8ee5b26e5701c58d
RodneyKoolman <rodney_koolman@hotmail.com>
2021-10-13 13:21:51 +0200

    Refactored code, removed settings.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 # LeasePlan settings
-leaseplan_brand = 'tesla'
-leaseplan_mileage = 10000
-leaseplan_duration = 60
-leaseplan_model = 'model-3'
-leaseplan_filters = 'b3eb0313-9583-427d-9db2-782f29f83afb'
+leaseplan_search_brand = 'tesla'
+leaseplan_search_model = 'model-3'
+leaseplan_search_filters = 'b3eb0313-9583-427d-9db2-782f29f83afb'
+leaseplan_contract_mileage = 10000
+leaseplan_contract_duration = 60
 
 # Scraper settings
 scraper_processed_vehicles = []
@@ -25,7 +25,7 @@ scraper_sendgrid_apikey = "[change_this]"
 scraper_sendgrid_from = "[change_this]"
 scraper_sendgrid_to = "[change_this]"
 scraper_base_domain = 'https://www.leaseplan.com'
-scraper_base_url = f'{scraper_base_domain}/nl-nl/zakelijk-leasen/showroom/{leaseplan_brand}/?leaseOption[mileage]={leaseplan_mileage}&leaseOption[contractDuration]={leaseplan_duration}&popularFilters={leaseplan_filters}&makemodel={leaseplan_model}'
+scraper_start_url = f'{scraper_base_domain}/nl-nl/zakelijk-leasen/showroom/{leaseplan_search_brand}/?leaseOption[mileage]={leaseplan_contract_mileage}&leaseOption[contractDuration]={leaseplan_contract_duration}&popularFilters={leaseplan_search_filters}&makemodel={leaseplan_search_model}'
 scraper_last_run_time = '00:00:00'
 scraper_last_error_time = '00:00:00'
 scraper_last_error_message = 'none'
@@ -42,7 +42,7 @@ def webserver_start():
 
 @scraper_webserver.route('/')
 def index():
-    return render_template('index.html', lastrun=scraper_last_run_time, lasterror=scraper_last_error_time, runcounter=scraper_total_run_count, errorcounter=scraper_total_error_count, vehiclesinmemory=len(scraper_processed_vehicles) , lastaddedvehicle=scraper_last_new_car, mailssent=scraper_total_mails_sent, checkevery=scraper_check_every, servertime=datetime.now().strftime('%H:%M:%S'), lastaddedvehiclelink=scraper_last_new_link, vehiclebrand=leaseplan_brand, vehiclemodel=leaseplan_model, vehicleduration=leaseplan_duration, vehiclemileage=leaseplan_mileage, mailenabled=scraper_mail_enabled, firstrun=scraper_first_run, errormessage=scraper_last_error_message)
+    return render_template('index.html', lastrun=scraper_last_run_time, lasterror=scraper_last_error_time, runcounter=scraper_total_run_count, errorcounter=scraper_total_error_count, vehiclesinmemory=len(scraper_processed_vehicles) , lastaddedvehicle=scraper_last_vehicle, mailssent=scraper_total_mails_sent, checkevery=scraper_check_every, servertime=datetime.now().strftime('%H:%M:%S'), lastaddedvehiclelink=scraper_last_vehiclelink, vehiclebrand=leaseplan_search_brand, vehiclemodel=leaseplan_search_model, vehicleduration=leaseplan_contract_duration, vehiclemileage=leaseplan_contract_mileage, mailenabled=scraper_mail_enabled, firstrun=scraper_first_run, errormessage=scraper_last_error_message)
 
 def parse(page):
     page = requests.get(page, timeout=5)
@@ -57,14 +57,14 @@ def error(ex):
     print(f'Error {scraper_last_error_message}. Retrying soon')
     time.sleep(5)
 
-def mail(current_car, current_car_link):
+def mail(current_vehicle, current_vehiclelink):
     global scraper_total_mails_sent
 
     message = Mail(
     from_email = scraper_sendgrid_from,
     to_emails = scraper_sendgrid_to,
-    subject = f'New {leaseplan_brand} available with id {current_car}',
-    html_content = f'New <strong>{leaseplan_brand}</strong> available with id <a href={scraper_base_domain+current_car_link}><strong>{current_car}</strong></a>.')
+    subject = f'New {leaseplan_search_brand} available with id {current_vehicle}',
+    html_content = f'New <strong>{leaseplan_search_brand}</strong> available with id <a href={scraper_base_domain+current_vehiclelink}><strong>{current_vehicle}</strong></a>.')
 
     try:
         sendgrid = SendGridAPIClient(scraper_sendgrid_apikey)
@@ -73,39 +73,39 @@ def mail(current_car, current_car_link):
 
     except Exception as ex:
         error(ex)
-        mail(current_car, current_car_link)
+        mail(current_vehicle, current_vehiclelink)
 
 def main():
     global scraper_total_run_count, scraper_last_run_time, scraper_last_vehicle, scraper_last_vehiclelink, scraper_first_run
 
     while True:
         try:
-            parsed_page = parse(scraper_base_url)
-            cars = parsed_page.find_all('div', {'data-component':'VehicleCard'})
+            parsed_page = parse(scraper_start_url)
+            vehicles = parsed_page.find_all('div', {'data-component':'VehicleCard'})
 
-            for car in cars:
-                current_car = car['data-key']
-                current_car_link = car.find("a")['data-e2e-id']
+            for vehicle in vehicles:
+                current_vehicle = vehicle['data-key']
+                current_vehiclelink = vehicle.find("a")['data-e2e-id']
 
-                if current_car not in scraper_processed_vehicles:                    
+                if current_vehicle not in scraper_processed_vehicles:                    
                     
-                    #Experimental - Not functional yet, set scraper_follow_cars to false
+                    #Experimental - Not functional yet, set scraper_follow_vehicles to false
                     if(scraper_follow_vehicles):
-                        page = parse(scraper_base_domain + current_car_link)
+                        page = parse(scraper_base_domain + current_vehiclelink)
                         specifications = page.find_all('div', {'data-component':'Specification'})
                         print(specifications)
 
                     if(scraper_mail_enabled and not scraper_first_run):
-                        mail(current_car, current_car_link)
+                        mail(current_vehicle, current_vehiclelink)
 
-                    scraper_processed_vehicles.append(current_car)
-                    scraper_last_vehicle = current_car
-                    scraper_last_vehiclelink = scraper_base_domain+current_car_link
+                    scraper_processed_vehicles.append(current_vehicle)
+                    scraper_last_vehicle = current_vehicle
+                    scraper_last_vehiclelink = scraper_base_domain+current_vehiclelink
                     time.sleep(2)
 
-            scraper_total_run_count += 1
             scraper_first_run = False
-            scraper_last_run = datetime.now().strftime('%H:%M:%S')
+            scraper_total_run_count += 1
+            scraper_last_run_time = datetime.now().strftime('%H:%M:%S')
 
             print('Done scraping. Processed vehicles are {}'.format(scraper_processed_vehicles))
 
